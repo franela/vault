10656cf0f1af8620890e462b05ecd94d4b1fd633
Jonathan Leibiusky <ionathan@gmail.com>
2014-10-23 00:25:13 -0400

    Fails if file to encrypt doesn't exist or cannot be accesed

--- commands/set/set_test.go
+++ commands/set/set_test.go
@@ -86,7 +86,20 @@ func TestSet(t *testing.T) {
 				g.Assert(code).Equal(3)
 			})
 
-			g.It("Should fail if file to encrypt doesn't exist or cannot be accesed")
+			g.It("Should fail if file to encrypt doesn't exist or cannot be accesed", func() {
+				testutils.SetTestGPGHome("bob")
+
+				v := &vault.Vaultfile{}
+				v.Recipients = []string{"bob@example.com"}
+				v.Save()
+
+				c, _ := Factory()
+
+				code := c.Run([]string{"-f", path.Join("some", "crazy", "unexisting", "path"), "set_test"})
+
+				g.Assert(code).Equal(1)
+			})
+
 			g.It("Should fail if encrypted file cannot be saved")
 			g.It("Should fail if encrypted file path starts with '..' or '/'")
 		})
