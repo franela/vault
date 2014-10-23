512b957ef7444d567fb9b6a4d1e6518c37718974
Jonathan Leibiusky <ionathan@gmail.com>
2014-10-22 23:48:17 -0400

    Fix bug in add command. It was not adding recipients to an empty Vaultfile

--- commands/add/add_test.go
+++ commands/add/add_test.go
@@ -65,6 +65,20 @@ func TestAdd(t *testing.T) {
 				g.Assert(newVaultfile.Recipients).Equal([]string{"bob@example.com", "alice@example.com"})
 				g.Assert(repairCommand.RunCalled).IsTrue()
 			})
+
+			g.It("Should add recipients to empty Vaultfile", func() {
+				c, _ := Factory()
+
+				repairCommand := cli.MockCommand{}
+				addCmd, _ := c.(addCommand)
+				addCmd.Repair = &repairCommand
+
+				addCmd.Run([]string{"alice@example.com"})
+
+				newVaultfile, _ := vault.LoadVaultfile()
+				g.Assert(newVaultfile.Recipients).Equal([]string{"alice@example.com"})
+				g.Assert(repairCommand.RunCalled).IsTrue()
+			})
 		})
 	})
 }
